43ab26c685f1caa9425ae37b859c38d46a2508ee
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-02-12 09:16:21 +0000

    Add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Grid, GridItem, Text } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
 
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <Grid templateAreas={`"header" "main" "footer"`}>
-      <GridItem bg={"red"} area={'header'}>Header</GridItem>
+      <GridItem bg={"black"} area={'header'}><NavBar /></GridItem>
       <GridItem bg={"blue"} area={'main'}>Main</GridItem>
       <GridItem bg={"gold"} area={'footer'}>Footer</GridItem>
     </Grid>
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,20 @@
+import { HStack, Image, Text, VStack } from "@chakra-ui/react"
+
+import logo from "../assets/logo.webp"
+
+const NavBar = () => {
+    return (
+        <>
+            <Text color='white' align='right'>Toggle</Text>
+            <VStack pb='10px'>
+                <Image src={logo} alt='logo' boxSize='60px' />
+                <VStack spacing={0}>
+                    <Text color='white'>Yan Highjack</Text>
+                    <Text color='white'>Photography is a poetry for an eye</Text>
+                </VStack>
+            </VStack>
+        </>
+    )
+}
+
+export default NavBar
