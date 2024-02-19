5e84317828ed61874454d5c5fa93ec11352fe206
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-02-19 21:58:27 +0000

    Update text in lading page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,26 @@
-import { Image, Text, VStack } from "@chakra-ui/react"
+import { Center, Image, Text, VStack } from "@chakra-ui/react"
 
 import logo from "../assets/logo.webp"
 
 const NavBar = () => {
     return (
         <>
-            <Text color='white' align='right'>Toggle</Text>
-            <VStack pb='10px'>
-                <Image src={logo} alt='logo' boxSize='60px' />
-                <VStack spacing={0}>
-                    <Text color='white'>Yen Highjack</Text>
-                    <Text color='white'>Photography is a poetry for an eye</Text>
+            <Text color='white' align='right' h='2vh'>Toggle</Text>
+            <Center h='98vh'>
+                <VStack pb='10px'>
+                    <Image src={logo} alt='logo' boxSize='150px' />
+                    <VStack spacing={0} style={{ fontFamily: 'Ivar Text' }}>
+                        <Text color='white' style={{
+                            letterSpacing: '7px',
+                            fontSize: '4.6rem'
+                        }}>Yen Highjack</Text>
+                        <Text color='white' style={{
+                            letterSpacing: '2px',
+                            fontSize: '1.6rem'
+                        }}>Photography is a poetry for an eye</Text>
+                    </VStack>
                 </VStack>
-            </VStack>
+            </Center >
         </>
     )
 }
