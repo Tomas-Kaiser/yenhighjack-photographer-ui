556921c1a754f6f54220f724779af36336e01af5
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-02-29 20:35:16 +0000

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,31 @@
-import { VStack, Text } from "@chakra-ui/react"
+import { VStack, Text, useBreakpointValue } from "@chakra-ui/react"
+import { useState } from "react";
 import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
+    const [hover, setHover] = useState(false);
+
     const date = new Date();
+
+
+    const instaIconStyle = {
+        fontSize: '2.6rem',
+        cursor: 'pointer',
+        borderRadius: '10px',
+        background: hover ? 'red' : 'transparent'
+    }
+
+    const instaIconSize = useBreakpointValue({ base: "40px", md: "50px" });
 
     return (
         <VStack justifyContent="center" p={3} bg="gray">
-            <FaInstagram color="white" size={30} />
+            <a href="https://www.instagram.com/yenhighjack"><FaInstagram
+                color="white"
+                size={instaIconSize}
+                style={instaIconStyle}
+                onMouseEnter={() => setHover(true)}
+                onMouseLeave={() => setHover(false)} />
+            </a>
             <Text color="white">&copy; {date.getFullYear()} Tomas Kaiser</Text>
         </VStack>
     )
