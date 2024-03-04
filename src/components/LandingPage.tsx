8cc40b6bbef571b6f14c932b2ad1f665833b7940
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-03-04 21:43:20 +0000

    Add a landingPage

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,22 @@
+import { Center, VStack, Image, Text } from "@chakra-ui/react"
+import logo from "../assets/logo.webp"
+import PhotoGrid from "./PhotoGrid"
+
+const LandingPage = () => {
+    return (
+        <>
+            <Center bg='black' h='100vh'>
+                <VStack pb='10px'>
+                    <Image src={logo} alt='logo' boxSize='150px' />
+                    <VStack spacing={0} style={{ fontFamily: 'Ivar Text' }}>
+                        <Text color='white' fontSize={{ base: '2.6rem', md: '4.6rem' }} letterSpacing={{ base: '4px', md: '7px' }}>Yen Highjack</Text>
+                        <Text color='white' fontSize={{ base: '1.2rem', md: '1.6rem' }} letterSpacing={{ base: '1.5px', md: '2px' }}>Photography is a poetry for an eye</Text>
+                    </VStack>
+                </VStack>
+            </Center >
+            <PhotoGrid />
+        </>
+    )
+}
+
+export default LandingPage
--- src/components/routes.tsx
+++ src/components/routes.tsx
@@ -2,14 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import AboutPage from "./AboutPage";
 import ContactPage from "./ContactPage";
-import PhotoGrid from "./PhotoGrid";
+import LandingPage from "./LandingPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
         children: [
-            { index: true, element: <PhotoGrid /> },
+            { index: true, element: <LandingPage /> },
             { path: 'about', element: <AboutPage /> },
             { path: 'contact', element: <ContactPage /> }
         ]
