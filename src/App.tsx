2f09833bce2d7ef260eda5fb304ad8692c6b2fdc
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-03-04 21:43:45 +0000

    Fix layout for the app

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import { Outlet } from 'react-router-dom'
 function App() {
 
   return (
-    <Grid templateAreas={`"header" "main" "footer"`}>
-      <GridItem bg={"black"} area={'header'}>
-        <NavBar />
-      </GridItem>
+    <Grid templateAreas={`"nav" "main" "footer"`}
+      templateRows="auto 1fr auto"
+      minHeight="100vh">
+      <GridItem area={'nav'}><NavBar /></GridItem>
       <GridItem area={'main'}><Outlet /></GridItem>
       <GridItem area={'footer'}><Footer /></GridItem>
     </Grid>
