ce67f849bcdab199792344e1b4b18646bea307fe
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-03-08 08:54:34 +0000

    Redo photoGrid component

--- src/components/PhotoGrid.tsx
+++ src/components/PhotoGrid.tsx
@@ -1,97 +1,74 @@
-import { SimpleGrid, Image, Box } from "@chakra-ui/react"
-import photo1 from "../../public/IMG_5122.jpg"
-import photo2 from "../../public/IMG_5123.jpg"
-// import photo3 from "../../public/IMG_5124.jpg"
-// import photo4 from "../../public/IMG_5125.jpg"
-import photo5 from "../../public/IMG_5126.jpg"
-// import photo6 from "../../public/IMG_5127.jpg"
-import photo7 from "../../public/IMG_5128.jpg"
-import photo8 from "../../public/IMG_5129.jpg"
-import photo9 from "../../public/IMG_5130.jpg"
-import photo10 from "../../public/IMG_5131.jpg"
-import photo11 from "../../public/IMG_5132.jpg"
-import photo12 from "../../public/IMG_5133.jpg"
-import photo13 from "../../public/IMG_5134.jpg"
-import photo14 from "../../public/IMG_5135.jpg"
-import photo15 from "../../public/IMG_5136.jpg"
-import photo16 from "../../public/IMG_5137.jpg"
-import photo17 from "../../public/IMG_5138.jpg"
+import { Image, Box } from "@chakra-ui/react"
 
 const photos = [
-    {
-        path: '../../public/IMG_5122.jpg'
-    },
-    {
-        path: '../../public/IMG_5123.jpg'
-    },
-    {
-        path: '../../public/IMG_5124.jpg'
-    },
     // {
-    //     path: '../../public/IMG_5125.jpg'
+    //     path: '/IMG_5122_mpthrd_c_scale,w_300.webp'
+    // },
+    // {
+    //     path: '/IMG_5123_plzeqy_c_scale,w_300.webp'
+    // },
+    // {
+    //     path: '/IMG_5124_uibnz7_c_scale,w_300.webp'
+    // },
+    // {
+    //     path: '/IMG_5126_thse63_c_scale,w_1030.webp'
+    // },
+    // {
+    //     path: '/IMG_5128_fx8wkp_c_scale,w_842.webp'
+    // },
+    // {
+    //     path: '/IMG_5129_nftsqp_c_scale,w_1200.webp'
     // },
     {
-        path: '../../public/IMG_5126.jpg'
+        path: '/IMG_5130.webp'
     },
     {
-        path: '../../public/IMG_5126.jpg'
-    },
-    // {
-    //     path: '../../public/IMG_5127.jpg'
-    // },
-    {
-        path: '../../public/IMG_5128.jpg'
+        path: '/IMG_5131.webp'
     },
     {
-        path: '../../public/IMG_5129.jpg'
+        path: '/IMG_5132.webp'
     },
     {
-        path: '../../public/IMG_5130.jpg'
+        path: '/IMG_5133.webp'
     },
     {
-        path: '../../public/IMG_5131.jpg'
+        path: '/IMG_5134.webp'
     },
     {
-        path: '../../public/IMG_5132.jpg'
+        path: '/IMG_5135.webp'
     },
     {
-        path: '../../public/IMG_5133.jpg'
+        path: '/IMG_5136.webp'
     },
     {
-        path: '../../public/IMG_5134.jpg'
+        path: '/IMG_5137.webp'
     },
     {
-        path: '../../public/IMG_5135.jpg'
-    },
-    {
-        path: '../../public/IMG_5136.jpg'
-    },
-    {
-        path: '../../public/IMG_5137.jpg'
-    },
-    {
-        path: '../../public/IMG_5138.jpg'
+        path: '/IMG_5138.webp'
     },
 ]
 
+const outerBoxStyle = {
+    columnCount: { base: '2', lg: '3', xl: 4 },
+    columnGap: '1rem',
+    width: '100%'
+}
+
+const innerBoxStyle = {
+    display: 'inline-block',
+    width: '100%',
+    marginBottom: '1rem',
+}
+
 const PhotoGrid = () => {
-    photos.forEach((photo) => {
-        console.log(photo.path)
-    })
-
-
-
-    console.log(photos)
     return (
-        <SimpleGrid columns={3} spacing={2} padding='10px'>
-
+        <Box p={3} sx={outerBoxStyle}>
             {photos.map((photo) => (
-                <Box >
+                <Box style={innerBoxStyle}>
                     <Image src={photo.path}></Image>
                 </Box>
             ))}
-
-        </SimpleGrid>
+        </Box>
     )
 }
 
