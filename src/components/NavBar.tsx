b70ced91e853be8eee60512627b9270736c894a9
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-03-29 21:59:35 +0000

    Implement a feature to highlight a selected item in menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
+import { HStack, IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import { GrMenu } from "react-icons/gr"
 import { MdMailOutline, MdOutlineWorkHistory } from "react-icons/md"
@@ -6,6 +6,7 @@ import { MdMailOutline, MdOutlineWorkHistory } from "react-icons/md"
 import { Link } from "react-router-dom"
 
 const NavBar = () => {
+    const [active, setActive] = useState('home')
     const [hamburgerActive, setHamburgerActive] = useState(false);
     const [width, setWidth] = useState(window.innerWidth);
 
@@ -25,7 +26,11 @@ const NavBar = () => {
         <HStack style={{ position: 'absolute', width: '100%' }} h='50px' bgColor={'green.700'} justify='space-between' pl={10} pr={10}>
             {/* <Text>Toggle</Text> */}
             <div style={{ padding: '5px' }}>
-                <Link to='/' style={{ color: 'white' }}>Home</Link>
+                <Link
+                    to='/'
+                    style={{ padding: '15px', color: 'white', background: active === 'home' ? '#42b37e' : 'inherit' }}
+                    onClick={() => setActive('home')}
+                >Home</Link>
             </div>
 
             {width <= 900 ? <Menu>
@@ -39,14 +44,14 @@ const NavBar = () => {
                     onClick={() => setHamburgerActive(!hamburgerActive)}
                 />
                 <MenuList style={{ background: '#276749', color: 'white' }}>
-                    <MenuItem bg='#276749' _hover={{ bg: '#235c41' }} as={Link} to='/about' icon={<MdOutlineWorkHistory />}>About</MenuItem>
-                    <MenuItem bg='#276749' _hover={{ bg: '#235c41' }} as={Link} to='/contact' icon={<MdMailOutline />}>Contact</MenuItem>
+                    <MenuItem bg={active === 'about' ? '#42b37e' : '#276749'} _hover={{ bg: '#235c41' }} as={Link} to='/about' icon={<MdOutlineWorkHistory />} onClick={() => setActive('about')}>About</MenuItem>
+                    <MenuItem bg={active === 'contact' ? '#42b37e' : '#276749'} _hover={{ bg: '#235c41' }} as={Link} to='/contact' icon={<MdMailOutline />} onClick={() => setActive('contact')}>Contact</MenuItem>
                 </MenuList>
             </Menu> : <div style={{ padding: '5px' }}>
-                <Link to='/about' style={{ padding: '20px', color: 'white' }}>About</Link>
-                <Link to='/contact' style={{ color: 'white' }}>Contact</Link>
+                <Link to='/about' style={{ padding: '15px', color: 'white', background: active === 'about' ? '#42b37e' : 'inherit' }} onClick={() => setActive('about')}>About</Link>
+                <Link to='/contact' style={{ padding: '15px', color: 'white', background: active === 'contact' ? '#42b37e' : 'inherit' }} onClick={() => setActive('contact')}>Contact</Link>
             </div>}
-        </HStack>
+        </HStack >
     )
 }
 
