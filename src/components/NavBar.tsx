0eeaa8ee3d8614203dcd851f85777aaebb3217e7
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-04-10 22:22:28 +0100

    Update colors to gray theme

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,6 +10,7 @@ const NavBar = () => {
     const [active, setActive] = useState('home')
     const [hamburgerActive, setHamburgerActive] = useState(false);
     const [width, setWidth] = useState(window.innerWidth);
+    const darkGreen = '#176734';
 
     useEffect(() => {
         const handleResize = () => {
@@ -24,33 +25,33 @@ const NavBar = () => {
     }, []);
 
     return (
-        <HStack style={{ position: 'absolute', width: '100%' }} h='50px' bgColor={'green.700'} justify='space-between' pl={10} pr={10}>
+        <HStack style={{ position: 'absolute', width: '100%' }} h='50px' bgColor={'gray.50'} justify='space-between' pl={10} pr={10}>
             {/* <Text>Toggle</Text> */}
             <Box
                 as={Link}
                 to='/'
-                style={{ padding: '15px', color: 'white', background: active === 'home' ? '#42b37e' : 'inherit' }}
+                style={{ padding: '15px', color: darkGreen, background: active === 'home' ? '#E2E8F0' : 'inherit' }}
                 onClick={() => setActive('home')}
-            ><FaHome style={{ color: 'white', fontSize: '20px' }} /></Box>
+            ><FaHome style={{ fontSize: '20px' }} /></Box>
 
             {width <= 900 ? <Menu>
-                <MenuButton style={{ color: 'white' }}
+                <MenuButton style={{ color: darkGreen }}
                     as={IconButton}
                     aria-label='Options'
                     icon={<GrMenu />}
-                    _hover={{ bg: '#235c41' }}
-                    _expanded={{ bg: '#42b37e' }}
+                    _hover={{ bg: 'gray.200' }}
+                    _expanded={{ bg: 'gray.200' }}
                     variant='outline'
                     onClick={() => setHamburgerActive(!hamburgerActive)}
                 />
-                <MenuList style={{ background: '#276749', color: 'white' }}>
-                    <MenuItem bg={active === 'home' ? '#42b37e' : '#276749'} _hover={{ bg: '#235c41' }} as={Link} to='/' icon={<FaHome />} onClick={() => setActive('home')}>Home</MenuItem>
-                    <MenuItem bg={active === 'about' ? '#42b37e' : '#276749'} _hover={{ bg: '#235c41' }} as={Link} to='/about' icon={<MdOutlineWorkHistory />} onClick={() => setActive('about')}>About</MenuItem>
-                    <MenuItem bg={active === 'contact' ? '#42b37e' : '#276749'} _hover={{ bg: '#235c41' }} as={Link} to='/contact' icon={<MdMailOutline />} onClick={() => setActive('contact')}>Contact</MenuItem>
+                <MenuList style={{ background: '#F7FAFC', color: darkGreen }}>
+                    <MenuItem bg={active === 'home' ? '#E2E8F0' : '#F7FAFC'} _hover={{ bg: '#E2E8F0' }} as={Link} to='/' icon={<FaHome />} onClick={() => setActive('home')}>Home</MenuItem>
+                    <MenuItem bg={active === 'about' ? '#E2E8F0' : '#F7FAFC'} _hover={{ bg: '#E2E8F0' }} as={Link} to='/about' icon={<MdOutlineWorkHistory />} onClick={() => setActive('about')}>About</MenuItem>
+                    <MenuItem bg={active === 'contact' ? '#E2E8F0' : '#F7FAFC'} _hover={{ bg: '#E2E8F0' }} as={Link} to='/contact' icon={<MdMailOutline />} onClick={() => setActive('contact')}>Contact</MenuItem>
                 </MenuList>
             </Menu> : <div style={{ padding: '5px' }}>
-                <Link to='/about' style={{ padding: '15px', color: 'white', background: active === 'about' ? '#42b37e' : 'inherit' }} onClick={() => setActive('about')}>About</Link>
-                <Link to='/contact' style={{ padding: '15px', color: 'white', background: active === 'contact' ? '#42b37e' : 'inherit' }} onClick={() => setActive('contact')}>Contact</Link>
+                <Link to='/about' style={{ padding: '15px', color: darkGreen, background: active === 'about' ? '#E2E8F0' : 'inherit' }} onClick={() => setActive('about')}>About</Link>
+                <Link to='/contact' style={{ padding: '15px', color: darkGreen, background: active === 'contact' ? '#E2E8F0' : 'inherit' }} onClick={() => setActive('contact')}>Contact</Link>
             </div>}
         </HStack >
     )
