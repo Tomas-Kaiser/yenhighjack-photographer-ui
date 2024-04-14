1ce236c186a4165dc5423c4fddaa60ff2502e2df
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-04-14 16:19:50 +0100

    Update hover color for insta icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,8 +18,7 @@ const Footer = () => {
         fontSize: useBreakpointValue({ base: "40px", md: "50px" }),
         cursor: 'pointer',
         borderRadius: '10px',
-        color: instHover ? '#fff' : '#176734',
-        background: instHover ? 'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)' : 'transparent'
+        color: instHover ? '#d6249f' : '#176734',
     }
 
     return (
