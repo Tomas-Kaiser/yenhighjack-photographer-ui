76dfaf89740f34734117333753248dc7da82de4c
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-04-18 21:42:41 +0100

    Create a photos file

---
+++ src/assets/photos.tsx
@@ -0,0 +1,14 @@
+export const photosAlbums = [
+    {
+        name: 'JANINE MADE BY LOVE',
+        path: '/IMG_5130.webp'
+    },
+    {
+        name: 'INFINIMY',
+        path: '/IMG_5131.webp'
+    },
+    {
+        name: 'ANOTHER ONE',
+        path: '/IMG_5132.webp'
+    },
+];
