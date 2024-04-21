b2e07fd732a4520c56a2f7bb0c036a7934973dcf
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-04-21 19:24:51 +0100

    Refactor page for Album

--- src/components/AlbumPage.tsx
+++ src/components/AlbumPage.tsx
@@ -1,43 +1,22 @@
-import { Box, Heading, Image } from "@chakra-ui/react"
 import { useParams } from "react-router-dom"
+import { Text } from "@chakra-ui/react"
 import { photosBasedAlbum } from "../assets/photos";
-
-const outerBoxStyle = {
-    columnCount: { base: '2', lg: '3', xl: 4 },
-    columnGap: '5px',
-    padding: '0 12px',
-    width: '100%',
-    background: '#fff'
-}
-
-const innerBoxStyle = {
-    display: 'inline-block',
-    width: '100%',
-    cursor: 'pointer'
-}
+import PhotoGrid from "./PhotoGrid";
 
 
 const AlbumPage = () => {
     const params = useParams();
     console.log('params: ', params.id);
 
-    const handlePhotoClick = (index: number) => {
-        console.log('clicked photo', index);
-    }
-
     if (!params.id) return null;
-    photosBasedAlbum[+params.id].photos.map(photo => console.log(photo));
 
     return (
         <>
-            <Heading mt={20} as='h1'>{`This is an Album page for ${params.id}!`}</Heading>
-            <Box sx={outerBoxStyle}>
-                {photosBasedAlbum[+params.id].photos.map((photoPath, index) => (
-                    <Box key={index} style={innerBoxStyle} onClick={() => handlePhotoClick(index)}>
-                        <Image src={photoPath}></Image>
-                    </Box>
-                ))}
-            </Box>
+            <Text mt={20}></Text>
+            <PhotoGrid
+                heading={photosBasedAlbum[+params.id].name}
+                subHeading='Click on a photo to see it in full size'
+                photos={photosBasedAlbum[+params.id].photos} />
         </>
     )
 }
