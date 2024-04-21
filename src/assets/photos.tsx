62838dbc08e87d843f63ffe29bfd382a76710a0a
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-04-21 19:23:51 +0100

    Add photos

--- src/assets/photos.tsx
+++ src/assets/photos.tsx
@@ -1,4 +1,4 @@
-export const photosAlbums = [
+export const photoCoverAlbums = [
     {
         name: 'JANINE MADE BY LOVE',
         path: '/IMG_5130.webp'
@@ -13,41 +13,204 @@ export const photosAlbums = [
     },
 ];
 
+export const topPhotos = {
+    name: 'TOP 15',
+    photos: [
+        {
+            name: 'TOP 1',
+            path: '/Number1.jpg',
+        },
+        {
+            name: 'INFINIMY 2',
+            path: '/Number2.jpg',
+        },
+        {
+            name: 'INFINIMY 3',
+            path: '/Number3.jpg',
+        },
+        {
+            name: 'INFINIMY 4',
+            path: '/Number4.jpg',
+        },
+        {
+            name: 'INFINIMY 5',
+            path: '/Number5.jpg',
+        },
+        {
+            name: 'INFINIMY 6',
+            path: '/Number6.jpg',
+        },
+        {
+            name: 'INFINIMY 7',
+            path: '/Number7.jpg',
+        },
+        {
+            name: 'INFINIMY 8',
+            path: '/Number8.jpg',
+        },
+        {
+            name: 'INFINIMY 9',
+            path: '/Number9.jpg',
+        },
+        {
+            name: 'INFINIMY 10',
+            path: '/Number10.jpg',
+        },
+        {
+            name: 'INFINIMY 11',
+            path: '/Number11.jpg',
+        },
+        {
+            name: 'INFINIMY 12',
+            path: '/Number12.jpg',
+        },
+        {
+            name: 'INFINIMY 13',
+            path: '/Number13.jpg',
+        },
+        {
+            name: 'INFINIMY 14',
+            path: '/Number14.webp',
+        },
+        {
+            name: 'INFINIMY 15',
+            path: '/Number15.jpg',
+        },
+    ]
+}
+
 export const photosBasedAlbum = [
     {
         name: 'JANINE MADE BY LOVE',
         photos: [
-            '/Janinemadebylove1.jpg',
-            '/Janinemadebylove2.jpg',
-            '/Janinemadebylove3.jpg',
-            '/Janinemadebylove4.jpg',
-            '/Janinemadebylove5.jpg',
-            '/Janinemadebylove6.jpg',
-            '/Janinemadebylove7.jpg',
-            '/Janinemadebylove8.jpg',
-            '/Janinemadebylove9.jpg',
-            '/Janinemadebylove10.jpg',
-            '/Janinemadebylove11.jpg',
-            '/Janinemadebylove12.jpg',
-            '/Janinemadebylove13.jpg'
+            {
+                name: 'JANINE MADE BY LOVE 1',
+                path: '/Infinimy-1.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 2',
+                path: '/Janinemadebylove2.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 3',
+                path: '/Janinemadebylove3.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 4',
+                path: '/Janinemadebylove4.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 5',
+                path: '/Janinemadebylove5.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 6',
+                path: '/Janinemadebylove6.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 7',
+                path: '/Janinemadebylove7.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 8',
+                path: '/Janinemadebylove8.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 9',
+                path: '/Janinemadebylove9.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 10',
+                path: '/Janinemadebylove10.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 11',
+                path: '/Janinemadebylove11.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 12',
+                path: '/Janinemadebylove12.jpg',
+            },
+            {
+                name: 'JANINE MADE BY LOVE 13',
+                path: '/Janinemadebylove13.jpg',
+            },
         ]
     },
-    // {
-    //     name: 'INFINIMY',
-    //     photos: [
-    //         {
-    //             name: 'JANINE MADE BY LOVE',
-    //             path: '/IMG_5131.webp'
-    //         }
-    //     ]
-    // },
-    // {
-    //     name: 'ANOTHER ONE',
-    //     photos: [
-    //         {
-    //             name: 'JANINE MADE BY LOVE',
-    //             path: '/IMG_5132.webp'
-    //         }
-    //     ]
-    // },
+    {
+        name: 'INFINIMY',
+        photos: [
+            {
+                name: 'INFINIMY 1',
+                path: '/Infinimy-01.jpg',
+            },
+            {
+                name: 'INFINIMY 2',
+                path: '/Infinimy-02.jpg',
+            },
+            {
+                name: 'INFINIMY 3',
+                path: '/Infinimy-03.jpg',
+            },
+            {
+                name: 'INFINIMY 4',
+                path: '/Infinimy-04.jpg',
+            },
+            {
+                name: 'INFINIMY 5',
+                path: '/Infinimy-05.jpg',
+            },
+            {
+                name: 'INFINIMY 6',
+                path: '/Infinimy-06.jpg',
+            },
+            {
+                name: 'INFINIMY 7',
+                path: '/Infinimy-07.jpg',
+            },
+            {
+                name: 'INFINIMY 8',
+                path: '/Infinimy-08.jpg',
+            },
+            {
+                name: 'INFINIMY 9',
+                path: '/Infinimy-09.jpg',
+            },
+            {
+                name: 'INFINIMY 10',
+                path: '/Infinimy-10.jpg',
+            },
+            {
+                name: 'INFINIMY 11',
+                path: '/Infinimy-11.jpg',
+            },
+            {
+                name: 'INFINIMY 12',
+                path: '/Infinimy-12.jpg',
+            },
+            {
+                name: 'INFINIMY 13',
+                path: '/Infinimy-13.jpg',
+            },
+        ]
+    }
 ];
+
+// {
+//     name: 'INFINIMY',
+//     photos: [
+//         {
+//             name: 'JANINE MADE BY LOVE',
+//             path: '/IMG_5131.webp'
+//         }
+//     ]
+// },
+// {
+//     name: 'ANOTHER ONE',
+//     photos: [
+//         {
+//             name: 'JANINE MADE BY LOVE',
+//             path: '/IMG_5132.webp'
+//         }
+//     ]
+// },
