829b11f06c181c91e9c0e2a35d5c424d0a6d8350
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-04-28 16:16:26 +0100

    Add CTA button in landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,8 +1,10 @@
-import { Center, VStack, Text } from "@chakra-ui/react"
+import { Center, VStack, Text, Button } from "@chakra-ui/react"
 // import logo from "../assets/logo.webp"
+import { FaArrowRight } from "react-icons/fa";
 import PhotoGrid from "./PhotoGrid"
 import AlbumMenu from "./AlbumMenu"
 import { topPhotos } from "../assets/photos"
+import { Link } from "react-router-dom"
 
 const LandingPage = () => {
     return (
@@ -13,6 +15,7 @@ const LandingPage = () => {
                     <VStack spacing={0} style={{ fontFamily: 'Ivar Text' }}>
                         <Text color='#176734' fontSize={{ base: '2.6rem', md: '4.6rem' }} letterSpacing={{ base: '4px', md: '7px' }}>Yen Highjack</Text>
                         <Text color='#176734' fontSize={{ base: '1.2rem', md: '1.6rem' }} letterSpacing={{ base: '1.5px', md: '2px' }}>Photography is a poetry for an eye</Text>
+                        <Button as={Link} to="/contact" color='#176734' borderColor='#176734' size='lg' variant='outline' mt={45} _hover={{ shadow: '1px 1px gray' }} rightIcon={<FaArrowRight />}>Order Me</Button>
                     </VStack>
                 </VStack>
             </Center >
