204ce413c1e8b4383a121502b488d9c8427ce491
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-04-28 16:15:40 +0100

    Add breadcrumbs

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,7 +1,24 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react"
+import { FaChevronRight } from "react-icons/fa"
+import { Link } from "react-router-dom"
 
 const About = () => {
     return (
-        <div style={{ padding: '100px' }}>Under the construction</div>
+        <>
+            <Breadcrumb mt={20} pl={5} spacing='8px' separator={<FaChevronRight size='12px' color='#176734' />}>
+                <BreadcrumbItem>
+                    <BreadcrumbLink as={Link} to='/' color='#176734'>
+                        Home
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <BreadcrumbLink isCurrentPage color='#176734'>
+                        About
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+            </Breadcrumb>
+            <div style={{ padding: '100px' }}>Under the construction</div>
+        </>
     )
 }
 
--- src/components/AlbumPage.tsx
+++ src/components/AlbumPage.tsx
@@ -1,5 +1,6 @@
-import { useParams } from "react-router-dom"
-import { Text } from "@chakra-ui/react"
+import { Link, useParams } from "react-router-dom"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from "@chakra-ui/react"
+import { FaChevronRight } from "react-icons/fa"
 import { photosBasedAlbum } from "../assets/photos";
 import PhotoGrid from "./PhotoGrid";
 
@@ -12,7 +13,18 @@ const AlbumPage = () => {
 
     return (
         <>
-            <Text mt={20}></Text>
+            <Breadcrumb mt={20} pl={5} spacing='8px' separator={<FaChevronRight size='12px' color='#176734' />}>
+                <BreadcrumbItem>
+                    <BreadcrumbLink as={Link} to='/' color='#176734'>
+                        Home
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbItem>
+                    <BreadcrumbLink isCurrentPage color='#176734'>
+                        Album
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+            </Breadcrumb>
             <PhotoGrid
                 heading={photosBasedAlbum[+params.id].name}
                 subHeading='Click on a photo to see it in full size'
