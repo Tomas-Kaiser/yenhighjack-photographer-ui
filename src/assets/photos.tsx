71e64b9219c58b9dfcc07a36264c184155573d36
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-06-27 22:03:42 +0100

    Implement CDN for photos

--- src/assets/photos.tsx
+++ src/assets/photos.tsx
@@ -1,193 +1,193 @@
 export const photoCoverAlbums = [
-    {
-        name: 'JANINE MADE BY LOVE',
-        path: '/Janinemadebylove1.jpg'
-    },
-    {
-        name: 'INFINIMY',
-        path: '/Infinimy-06.jpg'
-    }
+  {
+    name: "JANINE MADE BY LOVE",
+    path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/6a65c9f7-e8db-413f-0cd2-2ddb092f0900/public",
+  },
+  {
+    name: "INFINIMY",
+    path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/9f1fc37e-1e2c-4d06-f390-2ea4b9a83d00/public",
+  },
 ];
 
 export const topPhotos = {
-    name: 'TOP 15',
-    photos: [
-        {
-            name: 'TOP 1',
-            path: '/Number1.jpg',
-        },
-        {
-            name: 'INFINIMY 2',
-            path: '/Number2.jpg',
-        },
-        {
-            name: 'INFINIMY 3',
-            path: '/Number3.jpg',
-        },
-        {
-            name: 'INFINIMY 4',
-            path: '/Number4.jpg',
-        },
-        {
-            name: 'INFINIMY 5',
-            path: '/Number5.jpg',
-        },
-        {
-            name: 'INFINIMY 6',
-            path: '/Number6.jpg',
-        },
-        {
-            name: 'INFINIMY 7',
-            path: '/Number7.jpg',
-        },
-        {
-            name: 'INFINIMY 8',
-            path: '/Number8.jpg',
-        },
-        {
-            name: 'INFINIMY 9',
-            path: '/Number9.jpg',
-        },
-        {
-            name: 'INFINIMY 10',
-            path: '/Number10.jpg',
-        },
-        {
-            name: 'INFINIMY 11',
-            path: '/Number11.jpg',
-        },
-        {
-            name: 'INFINIMY 12',
-            path: '/Number12.jpg',
-        },
-        {
-            name: 'INFINIMY 13',
-            path: '/Number13.jpg',
-        },
-        {
-            name: 'INFINIMY 14',
-            path: '/Number14.webp',
-        },
-        {
-            name: 'INFINIMY 15',
-            path: '/Number15.jpg',
-        },
-    ]
-}
-
-export const photosBasedAlbum = [
+  name: "TOP 15",
+  photos: [
     {
-        name: 'JANINE MADE BY LOVE',
-        photos: [
-            {
-                name: 'JANINE MADE BY LOVE 1',
-                path: '/Janinemadebylove1.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 2',
-                path: '/Janinemadebylove2.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 3',
-                path: '/Janinemadebylove3.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 4',
-                path: '/Janinemadebylove4.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 5',
-                path: '/Janinemadebylove5.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 6',
-                path: '/Janinemadebylove6.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 7',
-                path: '/Janinemadebylove7.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 8',
-                path: '/Janinemadebylove8.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 9',
-                path: '/Janinemadebylove9.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 10',
-                path: '/Janinemadebylove10.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 11',
-                path: '/Janinemadebylove11.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 12',
-                path: '/Janinemadebylove12.jpg',
-            },
-            {
-                name: 'JANINE MADE BY LOVE 13',
-                path: '/Janinemadebylove13.jpg',
-            },
-        ]
+      name: "TOP 1",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/869d3ea1-3e95-42b9-4ad8-cd43e7ddd400/public",
     },
     {
-        name: 'INFINIMY',
-        photos: [
-            {
-                name: 'INFINIMY 1',
-                path: '/Infinimy-01.jpg',
-            },
-            {
-                name: 'INFINIMY 2',
-                path: '/Infinimy-02.jpg',
-            },
-            {
-                name: 'INFINIMY 3',
-                path: '/Infinimy-03.jpg',
-            },
-            {
-                name: 'INFINIMY 4',
-                path: '/Infinimy-04.jpg',
-            },
-            {
-                name: 'INFINIMY 5',
-                path: '/Infinimy-05.jpg',
-            },
-            {
-                name: 'INFINIMY 6',
-                path: '/Infinimy-06.jpg',
-            },
-            {
-                name: 'INFINIMY 7',
-                path: '/Infinimy-07.jpg',
-            },
-            {
-                name: 'INFINIMY 8',
-                path: '/Infinimy-08.jpg',
-            },
-            {
-                name: 'INFINIMY 9',
-                path: '/Infinimy-09.jpg',
-            },
-            {
-                name: 'INFINIMY 10',
-                path: '/Infinimy-10.jpg',
-            },
-            {
-                name: 'INFINIMY 11',
-                path: '/Infinimy-11.jpg',
-            },
-            {
-                name: 'INFINIMY 12',
-                path: '/Infinimy-12.jpg',
-            },
-            {
-                name: 'INFINIMY 13',
-                path: '/Infinimy-13.jpg',
-            },
-        ]
-    }
+      name: "INFINIMY 2",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/0a35f2ac-423d-4377-5ea2-fd12e09e7f00/public",
+    },
+    {
+      name: "INFINIMY 3",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/53e90bfc-6c80-4f4a-1ab6-31f6cfe89b00/public",
+    },
+    {
+      name: "INFINIMY 4",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/050c65f5-8eca-4ec8-3f07-3b6145893c00/public",
+    },
+    {
+      name: "INFINIMY 5",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/61db7bee-1201-463b-98ab-07f11eca2000/public",
+    },
+    {
+      name: "INFINIMY 6",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/53122c20-b0f6-4ae7-25b9-4a325c5bda00/public",
+    },
+    {
+      name: "INFINIMY 7",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/c693adcd-d9e1-439f-e012-8cbffcd09a00/public",
+    },
+    {
+      name: "INFINIMY 8",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/98157aa4-d37a-4337-14a3-89a9681c2900/public",
+    },
+    {
+      name: "INFINIMY 9",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/7787e999-da2e-4bc1-6739-1d2349411100/public",
+    },
+    {
+      name: "INFINIMY 10",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/c694f715-1e30-4eec-9c61-046bc68f4e00/public",
+    },
+    {
+      name: "INFINIMY 11",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/d3f01cd0-19fe-43d3-a241-e67f135c0f00/public",
+    },
+    {
+      name: "INFINIMY 12",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/705d8351-9660-44af-e6af-5f1626d6c900/public",
+    },
+    {
+      name: "INFINIMY 13",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/08507d86-32b6-4a8b-ce45-c61a1f974300/public",
+    },
+    {
+      name: "INFINIMY 14",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/4b93adf5-bcc3-41f1-c4bf-f89aa110a200/public",
+    },
+    {
+      name: "INFINIMY 15",
+      path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/1d1479c4-e9db-42ca-5746-a18f3977fb00/public",
+    },
+  ],
+};
+
+export const photosBasedAlbum = [
+  {
+    name: "JANINE MADE BY LOVE",
+    photos: [
+      {
+        name: "JANINE MADE BY LOVE 1",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/6a65c9f7-e8db-413f-0cd2-2ddb092f0900/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 2",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/82c57836-cb03-4800-915d-1ce0126ae800/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 3",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/9b68f3f3-9796-4810-8a38-46aeeed81a00/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 4",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/f6a6c79d-77d1-4b8e-7e9b-3573d5382a00/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 5",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/65a13da5-cd19-4d66-3902-9c88079a9900/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 6",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/65c9897b-4771-40a4-7f39-29bcab158000/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 7",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/34743daa-3e98-4a25-43ce-c29491489e00/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 8",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/9b2e25e6-c475-4fee-d092-5a25d941a400/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 9",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/14789a56-e5e8-4429-6df9-c78ec4ccf000/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 10",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/a2af953e-b445-4291-01b9-790d8322f600/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 11",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/5fba4ab7-0615-43e2-0f47-cfc17eb69b00/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 12",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/0dad2acf-a79a-4ed5-044a-376e70c61700/public",
+      },
+      {
+        name: "JANINE MADE BY LOVE 13",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/89e598cb-7e58-45be-35ed-1e7b9e3a1d00/public",
+      },
+    ],
+  },
+  {
+    name: "INFINIMY",
+    photos: [
+      {
+        name: "INFINIMY 1",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/bc482303-6466-4b8e-8062-801b2a7b7400/public",
+      },
+      {
+        name: "INFINIMY 2",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/266fd571-4c8c-4a93-a10f-ab9d30949000/public",
+      },
+      {
+        name: "INFINIMY 3",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/7dca3a6a-9333-422c-035a-b520f7e43300/public",
+      },
+      {
+        name: "INFINIMY 4",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/7ed113cb-8425-49b5-6ed8-1b286462ad00/public",
+      },
+      {
+        name: "INFINIMY 5",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/8c30fa17-1ef0-4fa6-e2d2-05a8a1d57b00/public",
+      },
+      {
+        name: "INFINIMY 6",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/9f1fc37e-1e2c-4d06-f390-2ea4b9a83d00/public",
+      },
+      {
+        name: "INFINIMY 7",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/5f772624-1977-48a1-349a-c9e1d48e2d00/public",
+      },
+      {
+        name: "INFINIMY 8",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/58fe6665-63f0-4dff-f16b-e3785a633e00/public",
+      },
+      {
+        name: "INFINIMY 9",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/594470e0-7b49-440c-716e-1cecf07df200/public",
+      },
+      {
+        name: "INFINIMY 10",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/fe5a2e71-6b91-4349-a36b-e4337f15c500/public",
+      },
+      {
+        name: "INFINIMY 11",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/5b9be6f2-597d-4017-bc91-da281f7e2a00/public",
+      },
+      {
+        name: "INFINIMY 12",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/f56db2a1-e717-48a9-7647-4e414e645000/public",
+      },
+      {
+        name: "INFINIMY 13",
+        path: "https://imagedelivery.net/nGg_6H5MpzveW4sWn4-OFg/afc07cec-873a-4280-c07b-a9bfdde73000/public",
+      },
+    ],
+  },
 ];
