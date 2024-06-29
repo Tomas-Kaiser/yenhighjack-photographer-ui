f4f272ac908c7a105ff57d2ed214fc33ed5fdae9
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-06-29 09:07:50 +0100

    Add scroll smooth behaviour

--- src/components/AlbumMenu.tsx
+++ src/components/AlbumMenu.tsx
@@ -1,73 +1,97 @@
-import { Box, Center, Divider, Heading, Text, Image } from "@chakra-ui/react"
-import { Link } from "react-router-dom"
-import { photoCoverAlbums } from '../assets/photos'
-import { useTranslation } from "react-i18next"
+import { Box, Center, Divider, Heading, Text, Image } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { photoCoverAlbums } from "../assets/photos";
+import { useTranslation } from "react-i18next";
 
 const hoverImgStyle = {
-    cursor: "pointer",
-    background: "rgba(0, 0, 0, 0.9)"
-}
+  cursor: "pointer",
+  background: "rgba(0, 0, 0, 0.9)",
+};
 
 const outerBoxStyle = {
-    display: { sm: 'flex' },
-    justifyContent: { sm: 'center' },
-    padding: { sm: '0 12px' },
-    width: '100%',
-    background: '#fff'
+  display: { sm: "flex" },
+  justifyContent: { sm: "center" },
+  padding: { sm: "0 12px" },
+  width: "100%",
+  background: "#fff",
+};
+
+interface Props {
+  scrollDown: React.RefObject<HTMLDivElement>;
 }
 
-const AlbumMenu = () => {
-    const { t } = useTranslation();
+const AlbumMenu = ({ scrollDown }: Props) => {
+  const { t } = useTranslation();
 
-    return (
-        <>
-            <Box id='albums'></Box>
-            <Heading as='h2' size='xl' textAlign='center' mt={70} mb={5} color='#176734'>{t('albums')}</Heading>
-            <Text textAlign='center' color='#176734' mb={3}>{t('albumSubText')}</Text>
-            <Center>
-                <Box w='180px' pb={10}>
-                    <Divider style={{ borderColor: '#176734' }} />
+  return (
+    <>
+      <Box ref={scrollDown} id="albums"></Box>
+      <Heading
+        as="h2"
+        size="xl"
+        textAlign="center"
+        mt={70}
+        mb={5}
+        color="#176734"
+      >
+        {t("albums")}
+      </Heading>
+      <Text textAlign="center" color="#176734" mb={3}>
+        {t("albumSubText")}
+      </Text>
+      <Center>
+        <Box w="180px" pb={10}>
+          <Divider style={{ borderColor: "#176734" }} />
+        </Box>
+      </Center>
+      <Box sx={outerBoxStyle}>
+        {photoCoverAlbums.map((photo, index) => (
+          <Box
+            key={photo.path}
+            width={{ sm: "230px", lg: "350px" }}
+            p={{ sm: "10px" }}
+          >
+            <Link to={`/albums/${index}`}>
+              <Box
+                id={photo.path}
+                position="relative"
+                mb={3}
+                _hover={{
+                  "> div": {
+                    opacity: 0.8,
+                  },
+                  cursor: "pointer",
+                }}
+              >
+                <Image
+                  src={photo.path}
+                  objectFit="cover"
+                  width="100%"
+                  height="400px"
+                  _hover={hoverImgStyle}
+                ></Image>
+                <Box
+                  position="absolute"
+                  top="0"
+                  left="0"
+                  width="100%"
+                  height="100%"
+                  backgroundColor="rgba(0, 0, 0, 0.5)"
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  opacity="1"
+                  transition="opacity 1s"
+                >
+                  <Text color="white">- {photo.name} -</Text>
                 </Box>
-            </Center>
-            <Box sx={outerBoxStyle}>
-                {photoCoverAlbums.map((photo, index) => (
-                    <Box key={photo.path} width={{ sm: '230px', lg: '350px' }} p={{ sm: '10px' }}>
-                        <Link
-                            to={`/albums/${index}`} >
-                            <Box
-                                id={photo.path}
-                                position='relative'
-                                mb={3}
-                                _hover={{
-                                    "> div": {
-                                        opacity: 0.80,
-                                    },
-                                    cursor: "pointer",
-                                }}
-                            >
-                                <Image src={photo.path} objectFit='cover' width='100%' height='400px' _hover={hoverImgStyle}></Image>
-                                <Box
-                                    position="absolute"
-                                    top="0"
-                                    left="0"
-                                    width="100%"
-                                    height="100%"
-                                    backgroundColor="rgba(0, 0, 0, 0.5)"
-                                    display="flex"
-                                    alignItems="center"
-                                    justifyContent="center"
-                                    opacity="1"
-                                    transition="opacity 1s"
-                                >
-                                    <Text color="white">- {photo.name} -</Text>
-                                </Box>
-                            </Box>
-                        </Link>
-                    </Box>
-                ))}
-            </Box >
-        </>
-    )
-}
+              </Box>
+            </Link>
+          </Box>
+        ))}
+      </Box>
+    </>
+  );
+};
 
-export default AlbumMenu
+export default AlbumMenu;
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,35 +1,69 @@
-import { Center, VStack, Text, Button, Heading } from "@chakra-ui/react"
+import { Center, VStack, Text, Button, Heading, Box } from "@chakra-ui/react";
+import { useRef } from "react";
 
 // import logo from "../assets/logo.webp"
 import { FaArrowDown } from "react-icons/fa";
-import PhotoGrid from "./PhotoGrid"
-import AlbumMenu from "./AlbumMenu"
-import { topPhotos } from "../assets/photos"
+import PhotoGrid from "./PhotoGrid";
+import AlbumMenu from "./AlbumMenu";
+import { topPhotos } from "../assets/photos";
 import { useTranslation } from "react-i18next";
 
 const LandingPage = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
+  // Used to scroll to top of photos section
+  const topPhotosRef = useRef<null | HTMLDivElement>(null);
 
-    return (
-        <>
-            <Center bg='linear-gradient(#F7FAFC 80%, #fff 100%)' h='100vh'>
-                <VStack pb='10px'>
-                    {/* <Image src={logo} alt='logo' boxSize='150px' /> */}
-                    <VStack spacing={0} style={{ fontFamily: 'Ivar Text' }}>
-                        <Heading as='h1' color='#176734' fontSize={{ base: '2.6rem', md: '4.6rem' }} letterSpacing={{ base: '4px', md: '7px' }}>Yen Highjack</Heading>
-                        <Text color='#176734' fontSize={{ base: '1.2rem', md: '1.6rem' }} letterSpacing={{ base: '1.5px', md: '2px' }}>{t("welcomeSlogan")}</Text>
-                        <Button as='a' href='#topPhotos' color='#176734' borderColor='#176734' size='lg' variant='outline' mt={45} _hover={{ shadow: '1px 1px gray' }} leftIcon={<FaArrowDown />} rightIcon={<FaArrowDown />}>{t("ctaBtn")}</Button>
-                    </VStack>
-                </VStack>
-            </Center >
-            <PhotoGrid
-                heading={t("heading")}
-                subHeading={t("subHeading")}
-                photos={topPhotos.photos}
-            />
-            <AlbumMenu />
-        </>
-    )
-}
+  return (
+    <Box style={{ scrollBehavior: "smooth", overflowY: "scroll" }}>
+      <Center bg="linear-gradient(#F7FAFC 80%, #fff 100%)" h="100vh">
+        <VStack pb="10px">
+          {/* <Image src={logo} alt='logo' boxSize='150px' /> */}
+          <VStack spacing={0} style={{ fontFamily: "Ivar Text" }}>
+            <Heading
+              as="h1"
+              color="#176734"
+              fontSize={{ base: "2.6rem", md: "4.6rem" }}
+              letterSpacing={{ base: "4px", md: "7px" }}
+            >
+              Yen Highjack
+            </Heading>
+            <Text
+              color="#176734"
+              fontSize={{ base: "1.2rem", md: "1.6rem" }}
+              letterSpacing={{ base: "1.5px", md: "2px" }}
+            >
+              {t("welcomeSlogan")}
+            </Text>
+            <Button
+              as="a"
+              onClick={() =>
+                topPhotosRef.current?.scrollIntoView({
+                  behavior: "smooth",
+                })
+              }
+              color="#176734"
+              cursor="pointer"
+              borderColor="#176734"
+              size="lg"
+              variant="outline"
+              mt={45}
+              _hover={{ shadow: "1px 1px gray" }}
+              leftIcon={<FaArrowDown />}
+              rightIcon={<FaArrowDown />}
+            >
+              {t("ctaBtn")}
+            </Button>
+          </VStack>
+        </VStack>
+      </Center>
+      <PhotoGrid
+        heading={t("heading")}
+        subHeading={t("subHeading")}
+        photos={topPhotos.photos}
+      />
+      <AlbumMenu scrollDown={topPhotosRef} />
+    </Box>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
