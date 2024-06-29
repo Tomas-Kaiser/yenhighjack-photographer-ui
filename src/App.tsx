f9de024db0362a54f017c7412eab0839d2a89165
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-06-29 18:12:38 +0100

    Add homeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Grid, GridItem } from "@chakra-ui/react";
 
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import { Outlet } from 'react-router-dom'
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import HomeContext from "./state-management/Contexts/HomeContext";
 
 function App() {
+  const [active, setActive] = useState("home");
 
   return (
-    <Grid templateAreas={`"nav" "main" "footer"`}
+    <Grid
+      templateAreas={`"nav" "main" "footer"`}
       templateRows="auto 1fr auto"
-      minHeight="100vh">
-      <GridItem area={'nav'}><NavBar /></GridItem>
-      <GridItem area={'main'}><Outlet /></GridItem>
-      <GridItem area={'footer'}><Footer /></GridItem>
+      minHeight="100vh"
+    >
+      <HomeContext.Provider value={{ active, setActive }}>
+        <GridItem area={"nav"}>
+          <NavBar />
+        </GridItem>
+        <GridItem area={"main"}>
+          <Outlet />
+        </GridItem>
+        <GridItem area={"footer"}>
+          <Footer />
+        </GridItem>
+      </HomeContext.Provider>
     </Grid>
-  )
+  );
 }
 
-export default App
+export default App;
