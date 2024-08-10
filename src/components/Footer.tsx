735c4830a022cccdd600f7f790db3da91943353f
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-08-10 11:29:52 +0100

    Add a link to my LinkedIn profile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,55 +1,68 @@
-import { VStack, Text, useBreakpointValue, Box, HStack } from "@chakra-ui/react"
+import {
+  VStack,
+  Text,
+  useBreakpointValue,
+  Box,
+  HStack,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { useTranslation } from "react-i18next";
 
 const Footer = () => {
-    const [fbHover, setFbHover] = useState(false);
-    const [instHover, setInstaHover] = useState(false);
-    const { t } = useTranslation();
-    const date = new Date();
+  const [fbHover, setFbHover] = useState(false);
+  const [instHover, setInstaHover] = useState(false);
+  const { t } = useTranslation();
+  const date = new Date();
 
-    const fbIconStyle = {
-        fontSize: useBreakpointValue({ base: "30px", md: "50px" }),
-        cursor: 'pointer',
-        borderRadius: '10px',
-        color: fbHover ? '#1877F2' : '#176734',
-    }
+  const fbIconStyle = {
+    fontSize: useBreakpointValue({ base: "30px", md: "50px" }),
+    cursor: "pointer",
+    borderRadius: "10px",
+    color: fbHover ? "#1877F2" : "#176734",
+  };
 
-    const instaIconStyle = {
-        fontSize: useBreakpointValue({ base: "30px", md: "50px" }),
-        cursor: 'pointer',
-        borderRadius: '10px',
-        color: instHover ? '#d6249f' : '#176734',
-    }
+  const instaIconStyle = {
+    fontSize: useBreakpointValue({ base: "30px", md: "50px" }),
+    cursor: "pointer",
+    borderRadius: "10px",
+    color: instHover ? "#d6249f" : "#176734",
+  };
 
-    return (
-        <VStack justifyContent="center" mt={10} p={3} bg="#F7FAFC" h="14vh">
-            <HStack>
-                <Box
-                    as='a'
-                    p='10px'
-                    cursor='pointer'
-                    href="https://www.facebook.com/share/jpVp8s9n6sw2aGfd"
-                    target="_blank"
-                    onMouseEnter={() => setFbHover(true)}
-                    onMouseLeave={() => setFbHover(false)}>
-                    <FaFacebook style={fbIconStyle} />
-                </Box>
-                <Box
-                    as='a'
-                    p='10px'
-                    cursor='pointer'
-                    href="https://www.instagram.com/yenhighjack/?igsh=Yzl0eW1wMGkxN3po&utm_source=qr"
-                    target="_blank"
-                    onMouseEnter={() => setInstaHover(true)}
-                    onMouseLeave={() => setInstaHover(false)}>
-                    <FaInstagram style={instaIconStyle} />
-                </Box>
-            </HStack>
-            <Text color="#176734" mb={3}>&copy; {date.getFullYear()} Jan Hajek | {t('created')} Tomas</Text>
-        </VStack>
-    )
-}
+  return (
+    <VStack justifyContent="center" mt={10} p={3} bg="#F7FAFC" h="14vh">
+      <HStack>
+        <Box
+          as="a"
+          p="10px"
+          cursor="pointer"
+          href="https://www.facebook.com/share/jpVp8s9n6sw2aGfd"
+          target="_blank"
+          onMouseEnter={() => setFbHover(true)}
+          onMouseLeave={() => setFbHover(false)}
+        >
+          <FaFacebook style={fbIconStyle} />
+        </Box>
+        <Box
+          as="a"
+          p="10px"
+          cursor="pointer"
+          href="https://www.instagram.com/yenhighjack/?igsh=Yzl0eW1wMGkxN3po&utm_source=qr"
+          target="_blank"
+          onMouseEnter={() => setInstaHover(true)}
+          onMouseLeave={() => setInstaHover(false)}
+        >
+          <FaInstagram style={instaIconStyle} />
+        </Box>
+      </HStack>
+      <Text color="#176734" mb={3}>
+        &copy; {date.getFullYear()} Jan Hajek | {t("created")}{" "}
+        <a href="https://www.linkedin.com/in/tomas-kaiser-06/" target="_blank">
+          Tomas Kaiser
+        </a>
+      </Text>
+    </VStack>
+  );
+};
 
-export default Footer
+export default Footer;
