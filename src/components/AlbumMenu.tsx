8510054a271bd9afcbe374d4f99c0ce3b31cfbb7
Tomas Kaiser <tomas.kaiser06@gmail.com>
2024-10-31 22:07:55 +0000

    Fix layout for displaying album menu

--- src/components/AlbumMenu.tsx
+++ src/components/AlbumMenu.tsx
@@ -12,9 +12,10 @@ const hoverImgStyle = {
 };
 
 const outerBoxStyle = {
-  columnCount: { base: "1", sm: "2" },
-  columnGap: "5px",
-  padding: { sm: "0 12px" },
+  display: "inline-flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  columnGap: "0px",
   width: "100%",
   background: "#fff",
 };
@@ -31,30 +32,6 @@ const AlbumMenu = () => {
   return (
     <>
       <Box id="albums"></Box>
-      {/* <Center>
-        <Box w="90%" pb={1} mt={70}>
-          <Divider style={{ borderColor: "#176734" }} />
-        </Box>
-      </Center>
-      <Heading
-        as="h2"
-        size="xl"
-        textAlign="center"
-        mb={5}
-        mt="20px"
-        color="#176734"
-        letterSpacing={{ base: "2px", md: "3px" }}
-      >
-        {t("albums").toUpperCase()}
-      </Heading>
-      <Text textAlign="center" color="#176734" mb={3}>
-        {t("albumSubText").toUpperCase()}
-      </Text>
-      <Center>
-        <Box w="60%" pb={10} mt="8px">
-          <Divider style={{ borderColor: "#176734" }} />
-        </Box>
-      </Center> */}
       <Heading
         headingText={t("albums").toUpperCase()}
         subHeadingText={t("albumSubText").toUpperCase()}
@@ -63,19 +40,18 @@ const AlbumMenu = () => {
         {photoCoverAlbums.map((photo, index) => (
           <Box
             key={photo.path}
-            p={{ sm: "10px" }}
-            display="flex"
+            display="inline-flex"
+            flexGrow={1}
             justifyContent={"center"}
           >
             <Link
               as="button"
-              width={{ base: "100%", sm: "200px", md: "300px", lg: "350px" }}
+              width={{ base: "100%" }}
               onClick={() => hanlderClick(index)}
             >
               <Box
                 id={photo.path}
                 position="relative"
-                mb={3}
                 _hover={{
                   "> div": {
                     opacity: 0.8,
